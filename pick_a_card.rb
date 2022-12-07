c2595b9e918eafbad723a94f91c3594c03777001
alexandra <beregistini-2@yandex.ru>
2022-12-07 18:27:14 +0300

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,9 @@
 # encoding utf-8
+#
+# Программа, которая выбирает произвольную карту игральной колоды, версия 2.
+# Масти — юникод-символы.
 
 values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-suits = %w(Diamonds Hearts Clubs Spades)
+suits = %w(♦ ♠ ♥ ♣)
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
